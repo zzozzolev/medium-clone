16889dc2d4e84178dd5333b34c4ab56255ee0a90
zzlev <gpalektzja@gmail.com>
2021-07-03 23:19:05 +0900

    Add abstract Timestamped model

---
+++ medium_clone/medium_clone/common/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Timestamped(models.Model):
+    # Automatically set the field to now every time the object is saved.
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Automatically set the field to now when the object is first created.
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
