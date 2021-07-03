10723127374224285ffab7408a24f627d23af841
zzlev <30290423+zzozzolev@users.noreply.github.com>
2021-07-04 00:56:05 +0900

    Rename medium_clone to apps (#6)

--- medium_clone/apps/asgi.py
+++ medium_clone/apps/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for medium_clone project.
+ASGI config for apps project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.settings')
 
 application = get_asgi_application()
