ea52af217dd648c2c0155703c2243dbce566cc11
zzlev <gpalektzja@gmail.com>
2021-07-10 15:34:15 +0900

    Add test_not_same_email

--- medium_clone/apps/authentication/tests.py
+++ medium_clone/apps/authentication/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RegistrationTests(APITestCase):
+    def test_not_same_email(self):
+        """
+        Users never have same email.
+        """
+        data = {"username": "test", "password": "test1234",
+                "password2": "test1234", "Email": "test@test.com", "first_name": "test", "last_name": "test"}
+        url = reverse("register")
+        _ = self.client.post(url, data, format="json")
+        res = self.client.post(url, data, format="json")
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- medium_clone/apps/authentication/urls.py
+++ medium_clone/apps/authentication/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from .views import RegisterView
 
 urlpatterns = [
-    url(r"^register/", RegisterView.as_view())
+    url(r"^register/", RegisterView.as_view(), name="register")
 ]
