03bbd6a4271c607cf745ac94242ec3c0bb6463e8
zzlev <gpalektzja@gmail.com>
2021-07-10 20:28:52 +0900

    Add get-auth-token api

--- medium_clone/apps/authentication/urls.py
+++ medium_clone/apps/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import RegisterView
 
 urlpatterns = [
-    url(r"^register/", RegisterView.as_view(), name="register")
+    url(r"^register/", RegisterView.as_view(), name="register"),
+    url(r"^get-auth-token/", obtain_auth_token, name="get-auth-token")
 ]
