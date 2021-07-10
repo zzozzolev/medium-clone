9687d633d30720822560cfed2d89120b1494fb9a
zzlev <gpalektzja@gmail.com>
2021-07-10 15:35:31 +0900

    Sort imports

--- medium_clone/apps/authentication/serializers.py
+++ medium_clone/apps/authentication/serializers.py
@@ -1,9 +1,9 @@
-from typing import Type, Mapping, Any
+from typing import Any, Mapping, Type
 
-from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class RegisterSerializer(serializers.ModelSerializer):
--- medium_clone/apps/authentication/views.py
+++ medium_clone/apps/authentication/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
 
 
 class RegisterView(CreateAPIView):
