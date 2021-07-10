f499e4b1c2d5d4210127bfeb1b305fbe000a1747
zzlev <30290423+zzozzolev@users.noreply.github.com>
2021-07-10 20:32:02 +0900

    feature: Add get auth token api (#9)
    * Add rest_framework.authtoken to settings
    
    * Apply rest_framework.authtoken migration
    
    * Add get-auth-token api

--- medium_clone/apps/settings.py
+++ medium_clone/apps/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'apps.profiles',
     'apps.authentication'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
