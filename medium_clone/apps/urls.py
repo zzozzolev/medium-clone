a7c06d5e664c5b9a2e3802383208f2ed6da21dad
zzlev <30290423+zzozzolev@users.noreply.github.com>
2021-07-11 12:56:14 +0900

    Use path instead of url (#10)

--- medium_clone/apps/urls.py
+++ medium_clone/apps/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
-    url(r"^api/", include("apps.authentication.urls"))
+    path("api/", include("apps.authentication.urls"))
 ]
