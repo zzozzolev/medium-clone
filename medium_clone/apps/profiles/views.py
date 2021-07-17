dc6544f21a34fe6b3881d00cdc9f969a17c2b983
zzlev <gpalektzja@gmail.com>
2021-07-17 19:21:41 +0900

    Convert RetrieveAPIView to RetrieveUpdateAPIView

--- medium_clone/apps/profiles/views.py
+++ medium_clone/apps/profiles/views.py
@@ -1,18 +1,28 @@
-from rest_framework.permissions import AllowAny
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
+
+from apps.common.permissions import IsOwnerOrReadOnly
 
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileRetrieveView(RetrieveAPIView):
-    permission_classes = (AllowAny, )
+class ProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsOwnerOrReadOnly, )
     queryset = Profile.objects.select_related("user")
     serializer_class = ProfileSerializer
 
     def retrieve(self, request, username, *args, **kwargs):
         profile = self.queryset.get(user__username=username)
         serializer = self.serializer_class(profile)
+
+        return Response(serializer.data)
+
+    def update(self, request, username, *args, **kwargs):
+        profile = self.queryset.get(user__username=username)
+        self.check_object_permissions(request, profile)
+        serializer = self.serializer_class(instance=profile, data=request.data)
+        serializer.is_valid()
+        serializer.save()
 
         return Response(serializer.data)
