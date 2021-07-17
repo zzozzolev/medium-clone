9648b342cd15bb30776f690d3f29f9d0a3047fb8
zzlev <gpalektzja@gmail.com>
2021-07-17 19:21:02 +0900

    Add update to ProfileSerializer

--- medium_clone/apps/profiles/serializers.py
+++ medium_clone/apps/profiles/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -7,6 +8,22 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username")
     email = serializers.EmailField(source="user.email")
+
+    def update(self, instance, validated_data):
+        user = instance.user
+        user_data = validated_data.pop("user")
+
+        for key, value in user_data.items():
+            setattr(user, key, value)
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        with transaction.atomic():
+            instance.save()
+            user.save()
+
+        return instance
 
     class Meta:
         model = Profile
