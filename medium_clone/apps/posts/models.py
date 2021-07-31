fe83d3209ba2fea738522a1cb7fa89762911c706
zzozzolev <gpalektzja@gmail.com>
2021-07-31 17:41:23 +0900

    Add post model

--- medium_clone/apps/posts/models.py
+++ medium_clone/apps/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import Timestamped
+
+
+class Post(Timestamped):
+    author = models.ForeignKey(
+        "profiles.Profile", on_delete=models.CASCADE, related_name='posts')
+    # max_length=300 is heuristics.
+    title = models.CharField(db_index=True, max_length=300)
+    # slug field is used to api apth, so it should be unique.
+    slug = models.SlugField(db_index=True, max_length=300, unique=True)
+    body = models.TextField()
+    description = models.TextField()
