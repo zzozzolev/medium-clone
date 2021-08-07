987e13be561e1c592fd10a6852b3e66cf5cf92e4
zzlev <gpalektzja@gmail.com>
2021-08-08 01:30:18 +0900

    Add create to PostViewSet

--- medium_clone/apps/posts/views.py
+++ medium_clone/apps/posts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.common.permissions import IsOwnerOrReadOnly
+
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ViewSet):
+    # TODO: Add `slug` field as lookup.
+    permission_classes = (IsOwnerOrReadOnly, )
+    queryset = Post.objects.select_related("author", "author__user")
+    serializer_class = PostSerializer
+
+    def create(self, request):
+        # TODO: Make test for this case.
+        if "post" not in request.data:
+            raise Response(request.data, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.serializer_class(request.data["post"])
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
