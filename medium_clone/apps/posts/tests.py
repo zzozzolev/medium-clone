fd0d5e8bfa39fddfe009e2db872075a23deb2c22
zzlev <gpalektzja@gmail.com>
2021-08-08 22:22:00 +0900

    Add documentation to tests

--- medium_clone/apps/posts/tests.py
+++ medium_clone/apps/posts/tests.py
@@ -10,6 +10,9 @@ class PostSerializerTests(APITestCase):
         cls.serializer = PostSerializer()
 
     def test_slug_equal_length(self):
+        """
+        Test slug which has maximum length except for unique size.
+        """
         data = {
             "title": "a" *
             (PostSerializer.SLUG_MAX_LENGTH - PostSerializer.UNIQUE_SIZE)
@@ -19,12 +22,18 @@ class PostSerializerTests(APITestCase):
         self.assertEqual(len(data["slug"]), PostSerializer.SLUG_MAX_LENGTH)
 
     def test_slug_gt_length(self):
+        """
+        Test slug which has maximum length.
+        """
         data = {"title": "a" * PostSerializer.SLUG_MAX_LENGTH}
         self.serializer.set_slug_by_title(data)
 
         self.assertEqual(len(data["slug"]), PostSerializer.SLUG_MAX_LENGTH)
 
     def test_slug_last_hypen(self):
+        """
+        Test slug which has hypen at last part.
+        """
         data = {
             "title": "a" *
             (PostSerializer.SLUG_MAX_LENGTH - PostSerializer.UNIQUE_SIZE - 1)
