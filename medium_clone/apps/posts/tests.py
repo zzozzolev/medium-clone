c223489ec63c5d52f0980dc68d5a58f28bd306a5
zzlev <gpalektzja@gmail.com>
2021-08-08 22:14:52 +0900

    Add setUpTestData to remove duplicated instanciated serializer

--- medium_clone/apps/posts/tests.py
+++ medium_clone/apps/posts/tests.py
@@ -5,20 +5,22 @@ from .serializers import PostSerializer
 
 
 class PostSerializerTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.serializer = PostSerializer()
+
     def test_slug_equal_length(self):
         data = {
             "title": "a" *
             (PostSerializer.SLUG_MAX_LENGTH - PostSerializer.UNIQUE_SIZE)
         }
-        serializer = PostSerializer()
-        serializer.set_slug_by_title(data)
+        self.serializer.set_slug_by_title(data)
 
         self.assertEqual(len(data["slug"]), PostSerializer.SLUG_MAX_LENGTH)
 
     def test_slug_gt_length(self):
         data = {"title": "a" * PostSerializer.SLUG_MAX_LENGTH}
-        serializer = PostSerializer()
-        serializer.set_slug_by_title(data)
+        self.serializer.set_slug_by_title(data)
 
         self.assertEqual(len(data["slug"]), PostSerializer.SLUG_MAX_LENGTH)
 
@@ -28,7 +30,6 @@ class PostSerializerTests(APITestCase):
             (PostSerializer.SLUG_MAX_LENGTH - PostSerializer.UNIQUE_SIZE - 1)
         }
         data["title"] += "-"
-        serializer = PostSerializer()
-        serializer.set_slug_by_title(data)
+        self.serializer.set_slug_by_title(data)
 
         self.assertNotRegex(data["slug"], "--")
