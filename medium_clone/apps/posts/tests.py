d2b1b5f18bbda07b72cfe694fc6ed91b0282fc26
zzlev <gpalektzja@gmail.com>
2021-08-08 22:03:26 +0900

    Add test_slug_equal_length to PostSerializerTests

--- medium_clone/apps/posts/tests.py
+++ medium_clone/apps/posts/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostSerializerTests(APITestCase):
+    def test_slug_equal_length(self):
+        data = {
+            "title": "a" *
+            (PostSerializer.SLUG_MAX_LENGTH - PostSerializer.UNIQUE_SIZE)
+        }
+        serializer = PostSerializer()
+        serializer.set_slug_by_title(data)
+
+        self.assertEqual(len(data["slug"]), PostSerializer.SLUG_MAX_LENGTH)
