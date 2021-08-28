c4650c5c2b17db1ded1d942f9fde11819daa2226
zzlev <30290423+zzozzolev@users.noreply.github.com>
2021-08-28 23:52:02 +0900

    feature: Rename modified_at to updated_at (#32)
    * Rename modified_at to updated_at
    
    * Change modified_at to updated_at in serializer.

--- medium_clone/apps/common/models.py
+++ medium_clone/apps/common/models.py
@@ -5,7 +5,7 @@ class Timestamped(models.Model):
     # Automatically set the field to now every time the object is saved.
     created_at = models.DateTimeField(auto_now_add=True)
     # Automatically set the field to now when the object is first created.
-    modified_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
