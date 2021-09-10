ddd32ad527674e13be5db3fcf334269ac3966103
zzlev <gpalektzja@gmail.com>
2021-09-10 17:24:23 +0900

    Set SECRET_KEY to environment variable.

--- medium_clone/apps/settings.py
+++ medium_clone/apps/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ ROOT_DIR = BASE_DIR.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pyso820rtds9b774ibt81u7i3m55vzj1$sb*^d)%3izymcs6m$'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
